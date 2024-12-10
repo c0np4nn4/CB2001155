ffcf42582a1426078bf8f0bcf53d049fe8423a49
c0np4nn4 <c0np4nn4@gmail.com>
2024-12-11 02:19:28 +0900

    nit: http to https

--- client/src/Section/CityData.jsx
+++ client/src/Section/CityData.jsx
@@ -75,7 +75,7 @@ function CityData() {
       await fetchCityData();
 
       const geocodeResponse = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
       );
 
       const { lat, lon } = geocodeResponse.data[0];
